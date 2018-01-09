60aaf6f9177e925633e36ec303bd0b5b800019df
Abhijit R Bangera <abangera@juniper.net>
2018-01-09 11:29:27 +0530

    first simple program

---
+++ Easy_Python_If-Else.py
@@ -0,0 +1,13 @@
+'''
+Question: https://www.hackerrank.com/challenges/py-if-else/problem
+'''
+if __name__ == '__main__':
+    n = int(input())
+    if n%2==0 and n in range(2,6):
+        print ("Not Weird")
+    elif n%2==0 and n in range(6,21):
+        print ("Weird")
+    elif n%2==0 and n>20:
+        print ("Not Weird")
+    elif n%2!=0:
+        print ("Weird")
